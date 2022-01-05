49ebe8c22c59f1cbba6ca7abd23b158ad18280e5
BabosApple <65014354+BabosApple@users.noreply.github.com>
2022-01-05 15:46:12 +0800

    Add files via upload

--- src/MulkiAqi192/DoubleJump/Main.php
+++ src/MulkiAqi192/DoubleJump/Main.php
@@ -2,46 +2,36 @@
 
 namespace MulkiAqi192\DoubleJump;
 
-use pocketmine\Server;
-use pocketmine\Player;
-
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJumpEvent;
-
-use pocketmine\level\particle\FlameParticle;
-
+use pocketmine\world\particle\FlameParticle;
 use pocketmine\math\Vector3;
+use pocketmine\utils\TextFormat as TF;
 
 class main extends PluginBase implements Listener {
 
-    public function onEnable(){
+    public $modeon = [];
+
+    public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-
-    public $modeon = [];
 
     public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
 
         switch($cmd->getName()){
             case "djump":
                 if($sender instanceof Player){
-                    if($sender->hasPermission("djump.cmd")){
-                        if(isset($this->modeon[$sender->getName()])){
-                            unset($this->modeon[$sender->getName()]);
-                            $sender->sendMessage("§7[§aDouble§6Jump§7] §cDouble Jump deactived!");
-                            return true;
-                        } else {
-                            $this->modeon[$sender->getName()] = $sender->getName();
-                            $sender->sendMessage("§7[§aDouble§6Jump§7] §aDouble Jump actived!");
-                            return true;
-                        }
+                    if(isset($this->modeon[$sender->getName()])){
+                        unset($this->modeon[$sender->getName()]);
+                        $sender->sendMessage(TF::RED . "[DoubleJump] Double Jump deactived!");
+                        return true;
                     } else {
-                        $sender->sendMessage("§7[§aDouble§6Jump§7] §cYou dont have permission to use this command!");
+                        $this->modeon[$sender->getName()] = $sender->getName();
+                        $sender->sendMessage(TF::GREEN . "[DoubleJump] Double Jump actived!");
                         return true;
                     }
                 } else {
@@ -54,17 +44,16 @@ class main extends PluginBase implements Listener {
 
     public function onJump(PlayerJumpEvent $event){
         $player = $event->getPlayer();
-        $level = $player->getLevel();
-        $x = $player->getX();
-		$y = $player->getY();
-		$z = $player->getZ();
+        $level = $player->getWorld();
+        $x = $player->getPosition()->getX();
+        $y = $player->getPosition()->getY();
+        $z = $player->getPosition()->getZ();
         if(isset($this->modeon[$player->getName()])){
             $player->setMotion(new Vector3(0, 0.8, 0));
-            $level->addParticle(new FlameParticle($player));
-            $level->addParticle(new FlameParticle(new Vector3($x-0.3, $y, $z)));
-			$level->addParticle(new FlameParticle(new Vector3($x, $y, $z-0.3)));
-			$level->addParticle(new FlameParticle(new Vector3($x+0.3, $y, $z)));
-			$level->addParticle(new FlameParticle(new Vector3($x, $y, $z+0.3)));
+            $level->addParticle(new Vector3($x-0.3, $y, $z), new FlameParticle(new Vector3($x-0.3, $y, $z)));
+            $level->addParticle(new Vector3($x, $y, $z-0.3), new FlameParticle(new Vector3($x, $y, $z-0.3)));
+            $level->addParticle(new Vector3($x+0.3, $y, $z), new FlameParticle(new Vector3($x+0.3, $y, $z)));
+            $level->addParticle(new Vector3($x, $y, $z+0.3), new FlameParticle(new Vector3($x, $y, $z+0.3)));
         }
     }
 
